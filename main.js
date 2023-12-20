e3288a44212c5a4b710f3766c106dcb989ec3044
melovitor <vitormelobatista@hotmail.com>
2023-12-20 17:03:45 -0300

    Ajustando texto de modos.

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const longButton = document.querySelector('.app__card-button--longo')
 
 const banner = document.querySelector('.app__image')
 
+const title = document.querySelector('.app__title')
+const subtitle = document.querySelector('.-strong')
+
 
 focusButton.addEventListener('click', () => {
     handleChangeContext('foco')
@@ -22,4 +25,28 @@ longButton.addEventListener('click', () => {
 function handleChangeContext(context){
     html.setAttribute('data-contexto', context)
     banner.setAttribute('src', `/imagens/${context}.png`)
+
+    switch (context) {
+        case 'foco': 
+            title.innerHTML = `
+                Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+        break;
+
+        case 'descanso-curto': 
+            title.innerHTML = `
+                Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+        break;
+        case 'descanso-longo': 
+            title.innerHTML = `
+                Hora de voltar á superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>
+            `
+        break;
+
+    }
+
 }
